4c8df4459392a483f78aa2e4856ce28931f6800c
alvaro2302 <alvaro230297@hotmail.com>
2018-11-09 03:58:55 -0400

    Revert "Add score"

--- lib/casilla.rb
+++ lib/casilla.rb
@@ -6,6 +6,10 @@ class Casilla
         @LadoAbajo = false
         @LadoDerecha = false
         @LadoIzquierda = false
+
+     
+        
+
     end  
 
     def ladoArriba()
@@ -27,8 +31,11 @@ class Casilla
     def estado()
         return @estado
     end
+
+
     
     def obtenerLado(filaInicial, columnaInicial, filaFinal,columnaFinal)
+
         lado = ""
         baseFinalfila = @baseFila +1
         baseFinalColumna = @baseColumna +1
@@ -37,15 +44,26 @@ class Casilla
         if filaInicial == @baseFila and columnaInicial == @baseColumna
             if diferenciacolumna == 1 and diferenciafila==0 
                 lado = "arriba"
+
             end
+
             if diferenciacolumna == 0 and diferenciafila == 1
                 lado = "izquierda"
+
             end
+        else
+            
+        
+        
         end
+
         return lado
+
+
     end
 
     def seleccionarLado(lado)
+    
         case lado
         when "Arriba"
             @LadoArriba = true
@@ -56,18 +74,15 @@ class Casilla
         when "Izquierda"
             @LadoIzquierda = true
         end  
+       
     end
 
     def estaLLena()
         return @LadoArriba === true && @LadoAbajo === true && @LadoIzquierda === true && @LadoDerecha=== true
     end
 
-    def obtenerEstado
-        estadoCadena = "" 
-        estado = [@LadoArriba,@LadoAbajo,@LadoDerecha,@LadoIzquierda]
-        estado.each do |elemento|
-            estadoCadena = estadoCadena + elemento.to_s + " "    
-        end
-        return estadoCadena
-    end
+
+
+
+   
 end
