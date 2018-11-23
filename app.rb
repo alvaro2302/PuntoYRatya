73feec0d53fb68789cdd2f9378453873a1b126f9
alvaro2302 <alvaro230297@hotmail.com>
2018-11-22 20:21:49 -0400

    add Three player

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require './lib/tablero'
 require './lib/jugador'
 require './lib/game'
 class App < Sinatra::Base
-  tablero = Tablero.new(4,4)
+  tablero = Tablero.new(2,2)
 
   game = Game.new(tablero)
   enable :sessions
@@ -15,6 +15,9 @@ class App < Sinatra::Base
 
   get '/mostrarUserName' do
     erb :userName
+  end
+  get '/mostrarUserNameThree' do
+    erb :userNameThree
   end
 
 
@@ -58,6 +61,65 @@ class App < Sinatra::Base
     else
       erb:gameover
     end
+  end
+  get '/mostrarTablaVaciaThree' do  
+    game.selecionarCantidadDeJugadores(3)
+    game.darNombre(1,params[:first_user_name])
+    game.darNombre(2,params[:second_user_name])
+    game.darNombre(3,params[:three_user_name])
+
+    
+
+    @jugador1 = game.obtenerJugador(1)
+    @jugador2 = game.obtenerJugador(2)
+    @jugador3 = game.obtenerJugador(3)
+    @numeroJugadorActual = game.obtenerJugadorActual()
+    @jugadorActual = game.obtenerJugador(@numeroJugadorActual)
+    
+    
+    @tabla = game.generarTabla
+    erb :gameThree
+  end
+  
+  get '/gameThree' do
+    @fila = params[:fila].to_i
+    @columna = params[:columna].to_i
+    seleccion = params[:selection]
+    game.darJugada(@fila.to_i,@columna.to_i,seleccion)
+    
+
+    
+
+    @jugador1 = game.obtenerJugador(1)
+    @jugador2 = game.obtenerJugador(2)
+    @jugador3 = game.obtenerJugador(3)
+    @numeroJugadorActual = game.obtenerJugadorActual()
+    @jugadorActual = game.obtenerJugador(@numeroJugadorActual)
+    @tabla = game.generarTabla
+    @gameOver = game.GameOver
+    if @gameOver ==false
+      erb :gameThree
+    else
+      erb:gameover
+    end
+
+  end
+
+
+  get '/seleccionJugadores' do
+    erb :seleccion
+  end
+
+  post '/redirecionCantidad' do
+    seleccionado = params[:selection]
+    
+    if seleccionado == "2"
+      redirect '/mostrarUserName'
+    end
+    if seleccionado == "3"
+      redirect '/mostrarUserNameThree'
+    end
+
   end
 
   post '/reinicarJuego' do
