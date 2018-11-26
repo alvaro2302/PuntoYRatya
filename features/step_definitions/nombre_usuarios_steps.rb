1b426ac56382a1795a60cce0231652c244f09be6
joshnos <joshuanostas49@gmail.com>
2018-11-25 22:07:04 -0400

    add features

--- features/step_definitions/nombre_usuarios_steps.rb
+++ features/step_definitions/nombre_usuarios_steps.rb
@@ -2,6 +2,14 @@ Given("estoy en la pagina userName") do
     visit('/mostrarUserName')
   end
 
+Given("estoy en la pagina userNameThree") do
+  visit('/mostrarUserNameThree')
+end
+
+Given("estoy en la pagina userNameFour") do
+  visit('/mostrarUserNameFour')
+end
+
   Then("deberia ver un campo con id {string}") do |string|
     expect(page).to have_field(string)
   end
@@ -17,3 +25,4 @@ Given("estoy en la pagina userName") do
   When("ingreso el nombre {string} en el segundo campo con id {string}") do |value, field|
     fill_in field, with: value
   end
+
